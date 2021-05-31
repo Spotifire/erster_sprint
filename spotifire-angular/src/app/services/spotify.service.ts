f5bf0a64e1e855c423a445f103b8e4748c88f13e
kinasch <janik@schoenberg.name>
2021-05-31 04:26:41 +0200

    Access Token is always expired

--- spotifire-angular/src/app/services/spotify.service.ts
+++ spotifire-angular/src/app/services/spotify.service.ts
@@ -1,51 +1,67 @@
-import {Injectable,Input} from '@angular/core';
-import SpotifyWebApi from "spotify-web-api-node";
-import Auth from "../auth/auth";
-
+import { Injectable, Input } from '@angular/core';
+import SpotifyWebApi from 'spotify-web-api-node';
+import Auth from '../auth/auth';
 
 @Injectable()
-export class SpotifyService{
+export class SpotifyService {
   @Input() code: string;
   @Input() auth: Auth;
 
-  accessToken
-  testAuth
+  accessToken;
 
-  constructor(){
-    
-  }
+  constructor() {}
 
   ngOnInit() {
     this.accessToken = this.auth.getAuth(this.code);
   }
 
+  searchValueChanged(str: string) {
+    this.accessToken = localStorage.getItem('accessToken');
 
-
-  searchValueChanged(str: string){
-    this.accessToken = localStorage.getItem("accessToken");
-    console.log(localStorage.getItem("accessToken"));
     this.searchMusic(str);
   }
 
-  searchMusic(str: string){
+  searchMusic(str: string) {
     let spotifyApi = new SpotifyWebApi({
-      clientId: "454d352b3fd84985bea141355d73c17b",
-    })
+      clientId: '454d352b3fd84985bea141355d73c17b',
+    });
 
     if (!this.accessToken) {
-      console.error("No viable Access Token.");
-      return
+      console.error('No viable Access Token.');
+      return;
     }
-    spotifyApi.setAccessToken(this.accessToken)
+    spotifyApi.setAccessToken(this.accessToken);
 
-    spotifyApi.searchTracks(str).then((res : any) => {
+    spotifyApi
+      .searchTracks(str)
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  }
 
-      console.log(res)
-    }).catch((err : any) => {
+  getUserPlaylists() {
+    this.accessToken = localStorage.getItem('accessToken');
 
-      console.log(err)
-    })
-    
+    let spotifyApi = new SpotifyWebApi({
+      clientId: '454d352b3fd84985bea141355d73c17b',
+    });
+
+    if (!this.accessToken) {
+      console.error('No viable Access Token.');
+      return;
+    }
+    spotifyApi.setAccessToken(this.accessToken);
+
+    spotifyApi
+      .getMe()
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
   }
 }
-
--- spotifire-angular/src/app/sidebar/playlist-overview/playlist-overview.component.ts
+++ spotifire-angular/src/app/sidebar/playlist-overview/playlist-overview.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-playlist-overview',
   templateUrl: './playlist-overview.component.html',
-  styleUrls: ['./playlist-overview.component.css']
+  styleUrls: ['./playlist-overview.component.css'],
 })
 export class PlaylistOverviewComponent implements OnInit {
-
-  constructor() { }
+  constructor(private spotifyService: SpotifyService) {}
 
   // tslint:disable-next-line: ban-types
-  playlists: Array<{name: String, creator: String, cover: String}> = [
-    {name: 'Playlist One', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist two', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist three', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist ...', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist ..', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist .', creator: 'Creator', cover: 'COVER'},
-    {name: 'Playlist', creator: 'Creator', cover: 'COVER'},
-];
+  playlists: Array<{ name: String; creator: String; cover: String }> = [
+    { name: 'Playlist One', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist two', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist three', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist ...', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist ..', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist .', creator: 'Creator', cover: 'COVER' },
+    { name: 'Playlist', creator: 'Creator', cover: 'COVER' },
+  ];
 
   ngOnInit(): void {
+    this.spotifyService.getUserPlaylists();
   }
-
 }
